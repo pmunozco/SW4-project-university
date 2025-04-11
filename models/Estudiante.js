39b4c037569d46123d2de5747d2861df2badde65
Paula Munoz <pmunoz@sproutloud.com>
2025-04-10 19:26:41 -0500

    Mi primer commit en github

---
+++ models/Estudiante.js
@@ -0,0 +1,27 @@
+const db = require('../config/database');
+
+class Estudiante{
+    static async obtenerTodosEstudiantes(){
+        try{
+            const [estudiantes] = await db.query('SELECT * FROM estudiantes');
+            return estudiantes;
+        }
+        catch(error){
+            console.error('Error al obtener informacion de los estudiantes', error);
+            throw error;
+        }
+    }
+
+    static async obtenerEstudiantePorId(id){
+        try{
+            const [estudiantes] = await db.query('SELECT * FROM estudiantes WHERE id = ?',[id]);
+            return estudiantes[0];
+        }
+        catch(error){
+            console.error('Error al obtener informacion del estudiante por id:', error);
+            throw error;
+        }
+    }
+
+
+}
